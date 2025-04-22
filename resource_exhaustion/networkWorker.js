9396aee3870e0c268dcbcb9620dd953b9eee9ade
Mitchell Zhou <mitche.zhou1@gmail.com>
2025-04-22 00:47:45 -0400

    please

--- resource_exhaustion/networkWorker.js
+++ resource_exhaustion/networkWorker.js
@@ -1,7 +1,7 @@
 onmessage = function () {
     const start = Date.now();
     while (Date.now() - start < 10000) {
-      fetch("cryptojacking-C2-server").catch(() => {});
+      fetch("https://cryptoJackingC2Server.com/thisnotreal").catch(() => {});
     }
     postMessage("Network Worker Done");
   };
